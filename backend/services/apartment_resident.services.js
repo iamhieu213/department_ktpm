a458e6789003c832338ef28a3b47ebe6fb6e71a7
nhatbx4002 <nhatbui2004.work@gmail.com>
2025-03-28 12:37:12 +0700

    add resident to apartment

--- backend/services/apartment_resident.services.js
+++ backend/services/apartment_resident.services.js
@@ -14,24 +14,17 @@ class ApartmentResidentServices {
         const list = await Apart_ResModels.find({apartment_id : apartmentId})
             .populate("resident_id", "name dob gender status");
 
-        if(!list.length){
+        if(list.length === 0){
             throw new Error('No resident found in apartment ');
         }
 
         return list ;
     }
 
-    async addResident (apartmentId , residentId , role ) {
-        const apartment = await ApartmentServices.findApartmentbyId(apartmentId);
-        if(!apartment) {
-            throw new Error('Apartment not found');
-        }
+    async addResident (apartmentId , userdata ) {
+        const {name , dob , gender , phone , email  , status} = userdata;
 
-        const existingRes = await Apart_ResModels.findOne({resident_id : residentId , apartment_id : apartmentId})
-
-        if(existingRes){
-            throw new Error("Resident is already in this apartment");
-        }
+        const newResident = await ResidentServices.createResident({name , dob , gender , phone , email  , status});
 
         if(role === "Owner" ){
             const owner = await Apart_ResModels.findOne({resident_id: residentId , role : "Owner"});
@@ -40,11 +33,20 @@ class ApartmentResidentServices {
             }
         }
 
-        const newRes = await Apart_ResModels.create({apartment_id : apartmentId, resident_id:residentId , role : role});
-
-
+        const newRes = await Apart_ResModels.create({apartment_id : apartmentId, resident_id:newResident._id, role : role});
 
         return newRes;
+    }
+
+    async deleteResident (apartmentId , residentId) {
+        const res_apart = await Apart_ResModels.findOne({apartmentId, residentId});
+
+        if(res_apart === "Owner"){
+            throw new Error("Cannot delete owner");
+        }
+
+        const result = await Apart_ResModels.deleteOne({apartment_id : apartmentId , resident_id : residentId});
+        return result;
     }
 }
 
